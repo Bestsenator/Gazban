a65c7ec842c81435bbdd09df31b3107d7f293098
Bestsenator <senator136019@gmail.com>
2023-04-21 18:16:31 +0430

    update v3 - Web

--- Web/config/urls.py
+++ Web/config/urls.py
@@ -21,4 +21,5 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
+    path('api/', include('api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL[0])
